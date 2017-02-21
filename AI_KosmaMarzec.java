2d13059fbf31a2fdae544d9fe3522b7bc5a54be8
Emejpi <emejpi@gmail.com>
2017-02-21 17:26:00 +0100

    why do you make my live harder, u bitch

--- AI_KosmaMarzec.java
+++ AI_KosmaMarzec.java
@@ -13,22 +13,47 @@ import java.util.Random;
  * Created by Artur Kosma & Michal Marzec on 07.02.2017.
  * Our alghoritm contains MCTS technique in order to find best possible moves.
  */
+import java.util.concurrent.TimeUnit;
 
 public class AI_KosmaMarzec extends AbstractPlayerController
 {
+    int[][] valuesMap;
+    List<Point> possiblePoints;
+    Field enemyColour, ourColour;
+    List<Point> emptyPoints;
+    int[][] moveValues;
+    int[][] simulationsCounts;
+
     public AI_KosmaMarzec(long movementTime)
     {
         super(movementTime);
+
+        valuesMap = new int[9][9];
+        moveValues = new int[9][9];
+        simulationsCounts = new int[9][9];
+
+        SetupValuesMap();
+
+        emptyPoints = new ArrayList<>();
+
+        //Field[][] mapCopy = GetBoardState();
+
+        for(int x = 0; x < 9; x++)
+        {
+            for(int y = 0; y < 9; y++)
+            {
+                emptyPoints.add(new Point(x,y));
+            }
+        }
     }
 
     @Override
-    public Point MakeMove(Point lastOpponentMove)
-    {
+    public Point MakeMove(Point lastOpponentMove){
         // Get copy of the map.
         Field[][] mapCopy = GetBoardState();
 
-        // Container for the colours.
-        Field enemyColour, ourColour;
+        if(emptyPoints.size() < 81)
+            emptyPoints.remove(lastOpponentMove);
 
         // Get enemy colour.
         if(GetColour() == Field.WHITE)
@@ -42,50 +67,121 @@ public class AI_KosmaMarzec extends AbstractPlayerController
             ourColour = Field.BLACK;
         }
 
-        // Array of possible points.
-        List<Point> possiblePoints = new ArrayList<>();
+        //emptyPoints = new ArrayList<>();
 
-        // Fill the array of possible points.
-        for(int x = 0; x < mapCopy.length; x++)
+        //reset moves values and counts
+        for(int x = 0; x < 9; x++)
         {
-            for(int y = 0; y < mapCopy[0].length; y++)
+            for(int y = 0; y < 9; y++)
             {
-                if(IsMoveValid(new Point(x, y)))
-                {
-                    possiblePoints.add(new Point(x, y));
-                }
+                moveValues[x][y] = 0;
+                simulationsCounts[x][y] = 0;
             }
         }
+
+        // Array of possible points.
+        possiblePoints = new ArrayList<>();
+
+        // Fill the array of possible points.
+        for (Point move:emptyPoints)
+        {
+            if (IsMoveValid(move)) {
+                possiblePoints.add(move);
+            }
+        }
+
+        UpdateValuesMap(lastOpponentMove, enemyColour);
 
         // Best point container.
         Point bestMove = new Point(0, 0); // Default point.
 
         // Value container.
-        int[] moveValues = new int[possiblePoints.size()];
+        //int[] moveValues = new int[possiblePoints.size()];
+        /*moveValues = new ArrayList<>();
+        simulationsCounts = new ArrayList<>();
+        for (Point move:possiblePoints) {
+            moveValues.add(0);
+            simulationsCounts.add(0);
+        } */
 
         // Best value buffer.
         int bestValue = 0; // Default value.
+
+        if(emptyPoints.size() > 30) //valuation
+        CutPossibleMoves(2);
+
+        Log.d("Time Left", GetRemainingTimeMS() + "");
+
+
+        int cutDeferenceValue = 6;
+        int possibleSize = possiblePoints.size();
+
+        Point currentMove = new Point(0,0);
 
         // While we still have time left.
         while(GetRemainingTimeMS() >= 0)
         {
             Log.d("Simulation", "S T A R T");
+
             // Iterate through all possible points.
-            for(int x = 0; x < possiblePoints.size(); x++)
+            for(int x = 0; x < possibleSize; x++)
             {
+                currentMove = possiblePoints.get(x);
+
                 // Add value of simulation at specific point to an array of values.
-                moveValues[x] += Simulate(possiblePoints.get(x), enemyColour, ourColour, mapCopy);
-                Log.d("Simulation", "F I N I S H E D");
+                UpdateMoveValue(currentMove, Simulate(currentMove, enemyColour, ourColour, mapCopy));
+
+                //Log.d("Simulation", "move value: " + moveValues.get(x));
 
                 // Update the value buffer and the best point container.
-                if(moveValues[x] > bestValue)
+                if(moveValues[currentMove.x][currentMove.y] > bestValue)
                 {
-                    bestValue = moveValues[x];
+                    bestValue = moveValues[currentMove.x][currentMove.y];
                     bestMove = possiblePoints.get(x);
                 }
             }
+
+            //Extra cutting nie dziala odpowiednio po zmianie wartosci na usrednione wiec wypieprzamy narazie
+            /*for (int i = 0; i < possiblePoints.size(); i++) //Extra Cutting.
+            {
+                if(moveValues[possiblePoints.get(i).x][possiblePoints.get(i).y] < bestValue - cutDeferenceValue)
+                {
+                    //moveValues.remove(i);
+                    possiblePoints.remove(i);
+                    i--;
+                }
+            }*/
+
+            //nie pamietam czy to jest potrzebne poza Extra Cutem. Chyba można wyjebac ale nie jestem pewien xd
+            if(possibleSize > possiblePoints.size())
+            {
+                possibleSize = possiblePoints.size();
+
+                cutDeferenceValue ++;
+
+                Log.d("cutDeferenceValue", cutDeferenceValue + "");
+            }
+
+
+
+            Log.d("Time Left", GetRemainingTimeMS() + "");
+            Log.d("Simulation", "best move: " + bestMove);
         }
 
+
+        UpdateValuesMap(bestMove, ourColour);
+
+        emptyPoints.remove(bestMove);
+
+        Log.d("Is This Move Fine? ", IsMoveValid(bestMove)? "Yes" : "No" );
+
+        PrintCurrentMapStateToConsole(); //do usuniecia
+
+        try {
+            TimeUnit.SECONDS.sleep(1);   //do usuniecia
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         // Returns the best move found during specified time.
         return bestMove;
     }
@@ -93,14 +189,13 @@ public class AI_KosmaMarzec extends AbstractPlayerController
     // Simulates the game at specific point.
     private int Simulate(Point point, Field enemyColour, Field ourColour, Field[][] mapCopy)
     {
+        //Log.d("Time befor sim", GetRemainingTimeMS() + "");
+
         // Boolean deciding who's having the move in simulation.
         boolean enemy = true;
 
         // Create the random generator object.
         Random randomIndex = new Random();
-
-        // Container for a proper index from the possible moves lists.
-        int properIndex;
 
         // Create a new map for simulation.
         Field[][] mapSimulation = new Field[mapCopy.length][mapCopy[0].length];
@@ -123,93 +218,238 @@ public class AI_KosmaMarzec extends AbstractPlayerController
         // Create linked lists of possible points for both players in simulation.
         List<Point> enemyPossibleMoves = new LinkedList<>();
         List<Point> ourPossibleMoves = new LinkedList<>();
+        List<Point> chosenMoves = new LinkedList<>();
 
         // Fill lists of possible first moves for this simulation.
-        for(int x = 0; x < mapSimulation.length; x++)
+        for (Point move : emptyPoints)
         {
-            for(int y = 0; y < mapSimulation[0].length; y++)
-            {
-                if(gameSimulation.IsMoveValid(new Point(x, y), enemyColour))
-                {
-                    enemyPossibleMoves.add(new Point(x, y));
-                }
-
-                if(gameSimulation.IsMoveValid(new Point(x, y), ourColour))
-                {
-                    ourPossibleMoves.add(new Point(x, y));
-                    ourPossibleMoves.size();
-                }
-            }
+            enemyPossibleMoves.add(move);
+            ourPossibleMoves.add(move);
         }
+
+        Field currentPlayer = Field.BLACK;
+
+        int countMoves = 0;
+
+        // Container for a proper index from the possible moves lists.
+        int properIndex = 0;
 
         // Until simulation is over.
         while(true)
         {
+            //Sim end
+            if(++countMoves > 5)
+            {
+                RemoveUnmovable(gameSimulation, ourPossibleMoves, ourColour);
+                RemoveUnmovable(gameSimulation, enemyPossibleMoves, enemyColour);
+                //Log.d("Time after sim", GetRemainingTimeMS() + "");
+                int currentMoveValue = ourPossibleMoves.size() - enemyPossibleMoves.size();
+
+                /////////Tu trzeba zupować chosen move alpha-cos
+                for (Point move:chosenMoves)
+                {
+                    UpdateMoveValue(move, currentMoveValue);
+                }
+
+                return currentMoveValue;
+            }
+
             // Enemy move.
             if(enemy)
             {
-                // Search for a proper index.
-                while(true)
-                {
-                    // Generate random index.
-                    properIndex = randomIndex.nextInt(enemyPossibleMoves.size());
-
-                    // Delete unproper index.
-                    if (!gameSimulation.IsMoveValid(new Point(enemyPossibleMoves.get(properIndex)), enemyColour))
-                    {
-                        enemyPossibleMoves.remove(properIndex);
-
-                        // Enemy lose.
-                        if (enemyPossibleMoves.size() == 0)
-                        {
-                            return 1;
-                        }
-                    }
-
-                    else
-                    {
-                        break;
-                    }
-                }
-
-                // Put a pin on that location.
-                mapSimulation[enemyPossibleMoves.get(properIndex).x][enemyPossibleMoves.get(properIndex).y] = enemyColour;
+                if(SimulateSingleMove(gameSimulation, randomIndex, mapSimulation, enemyPossibleMoves, enemyColour).x < 0) //checks if there is no more possible moves
+                    countMoves = 5;
             }
 
             // Our move.
             else
             {
-                // Search for a proper index.
-                while(true)
-                {
-                    // Generate random index.
-                    properIndex = randomIndex.nextInt(ourPossibleMoves.size());
+                Point chosenMove = SimulateSingleMove(gameSimulation, randomIndex, mapSimulation, ourPossibleMoves, ourColour);
 
-                    // Delete unproper index.
-                    if (!gameSimulation.IsMoveValid(new Point(ourPossibleMoves.get(properIndex)), ourColour))
-                    {
-                        ourPossibleMoves.remove(properIndex);
-
-                        // We lose.
-                        if (ourPossibleMoves.size() == 0)
-                        {
-                            return 0;
-                        }
-                    }
-
-                    else
-                    {
-                        break;
-                    }
-                }
-
-                // Put a pin on that location.
-                mapSimulation[ourPossibleMoves.get(properIndex).x][ourPossibleMoves.get(properIndex).y] = ourColour;
+                if(chosenMove.x < 0)
+                    countMoves = 5;
+                else
+                    chosenMoves.add(chosenMove);
             }
 
             // Switch player.
             enemy = !enemy;
         }
+
+    }
+
+    void UpdateMoveValue(Point move, int newMoveValue)
+    {
+        simulationsCounts[move.x][move.y] += 1;
+        moveValues[move.x][move.y] *= (simulationsCounts[move.x][move.y] - 1) / simulationsCounts[move.x][move.y];
+        moveValues[move.x][move.y] += newMoveValue * ( 1 / simulationsCounts[move.x][move.y]);
+    }
+
+    Point SimulateSingleMove(Game gameSimulation, Random randomIndex, Field[][] mapSimulation, List<Point> possibleMoves, Field color)
+    {
+        // Search for a proper index.
+        int properIndex = 0;
+
+        while(true)
+        {
+            properIndex = randomIndex.nextInt(possibleMoves.size());
+
+            // Delete unproper index.
+            if (!gameSimulation.IsMoveValid(possibleMoves.get(properIndex), color))
+            {
+                possibleMoves.remove(properIndex);
+
+                // Enemy lose.
+                if (possibleMoves.size() == 0)
+                {
+                    return new Point(-1,-1); //taa wiem ze messy ale tak jest najsensowniej
+                }
+            }
+
+            else
+            {
+                // Put a pin on that location.
+                mapSimulation[possibleMoves.get(properIndex).x][possibleMoves.get(properIndex).y] = color;
+                return possibleMoves.get(properIndex);
+            }
+        }
+
+    }
+
+    void RemoveUnmovable(Game simulation, List<Point> moves, Field color)
+    {
+        for (int i = 0; i < moves.size(); i++)
+        {
+            if(!simulation.IsMoveValid(moves.get(i), color))
+            {
+                moves.remove(i--);
+            }
+        }
+    }
+
+    void SetupValuesMap() //w konstruktorze
+    {
+        for(int y = 0; y < valuesMap[0].length; y++)
+            for(int x = 0; x < valuesMap.length; x++)
+            {
+                valuesMap[x][y] = 0;
+                if(x==0 || x==valuesMap.length-1)
+                {
+                    valuesMap[x][y] += 2;
+                }
+                if(y==0 || y==valuesMap.length-1)
+                {
+                    valuesMap[x][y] += 2;
+                }
+            }
+    }
+
+    void UpdateValuesMap(Point point, Field color) //point - pozycja ostatniego ruchu, color - kolor ostatniego ruchu
+    {
+        if(color == GetColour())
+        {
+            //zmiejsza wartość na przyległych do naszego
+            AddValueToValuesMapStraight(point, -1, 1);
+
+            //zmniejsza wartość na oddalonych o 2 od naszego
+            AddValueToValuesMapStraight(point, -2, 2);
+            AddValueToValuesMapSlant(point, -2, 1);
+        }
+        else
+        {
+            //zwiększa wartość na przyległych do przeciwnika
+            AddValueToValuesMapStraight(point, 1, 1);
+
+            //zmiejsza wartość na oddalonych o 2 od przeciwnika
+            AddValueToValuesMapStraight(point, -1, 2);
+            AddValueToValuesMapSlant(point, -1, 1);
+        }
+    }
+
+    void CutPossibleMoves(int untrust) //im mniejszy untrust tym więcej ucina. Dla 0 ucina wszystkie mniejsze od maksymalnej wartosci
+    {
+        int valueMax = -1000;
+        int valueSecondMax = -1000;
+
+        //znalezienie najwiekszej wartosci ruchu
+
+        for (Point move:possiblePoints)
+        {
+            if(valuesMap[move.x][move.y] >= valueMax)
+            {
+                valueSecondMax = valueMax;
+                valueMax = valuesMap[move.x][move.y];
+            }
+        }
+
+        Log.d("Max Value", valueMax + "");
+
+        Log.d("moves befor", possiblePoints.size() + "");
+
+        //usunięcie ruchów o wartości mniejszej od maksymalnej o więcej niż parametr nieufności untrust
+        for(int i = 0; i < possiblePoints.size(); i++)
+        {
+            if(valuesMap[possiblePoints.get(i).x][possiblePoints.get(i).y] < valueSecondMax - untrust)
+            {
+                possiblePoints.remove(i);
+                i--;
+            }
+        }
+
+        Log.d("moves after", possiblePoints.size() + "");
+    }
+
+    void AddValueToValuesMapStraight(Point point, int value, int range)
+    {
+        if(point.x - range >= 0 )
+        {
+            valuesMap[point.x - range][point.y] += value;
+        }
+
+        if(point.x + range <= valuesMap.length - 1 )
+        {
+            valuesMap[point.x + range][point.y] += value;
+        }
+
+        if(point.y - range >= 0 )
+        {
+            valuesMap[point.x][point.y - range] += value;
+        }
+
+        if(point.y + range <= valuesMap.length - 1 )
+        {
+            valuesMap[point.x][point.y + range] += value;
+        }
+    }
+
+    void AddValueToValuesMapSlant(Point point, int value, int range)
+    {
+        if(point.x - range >= 0 )
+        {
+            if(point.y - range >= 0 )
+            {
+                valuesMap[point.x - range][point.y - range] += value;
+            }
+
+            if(point.y + range <= valuesMap.length - 1 )
+            {
+                valuesMap[point.x - range][point.y + range] += value;
+            }
+        }
+
+        if(point.x + range <= valuesMap.length - 1 )
+        {
+            if(point.y - range >= 0 )
+            {
+                valuesMap[point.x + range][point.y - range] += value;
+            }
+
+            if(point.y + range <= valuesMap.length - 1 )
+            {
+                valuesMap[point.x + range][point.y + range] += value;
+            }
+        }
+
     }
 
     // Prints the current map state to the console.
@@ -230,6 +470,39 @@ public class AI_KosmaMarzec extends AbstractPlayerController
                 }
 
                 else if(simulatedGame.GetBoardState()[i][n] == Field.BLACK)
+                {
+                    row += 'B';
+                }
+
+                else
+                {
+                    row += 'X';
+                }
+            }
+
+            // Debug and clear.
+            Log.d("Row number: " + rowNumber, row);
+            row = "";
+            rowNumber++;
+        }
+    }
+
+    private void PrintCurrentMapStateToConsole()
+    {
+        String row = "";
+        int rowNumber = 0;
+
+        // Fills the row string.
+        for(int i = 0; i < GetBoardState().length; i++)
+        {
+            for(int n = 0; n < GetBoardState().length; n++)
+            {
+                if(GetBoardState()[n][i] == Field.WHITE)
+                {
+                    row += 'W';
+                }
+
+                else if(GetBoardState()[n][i] == Field.BLACK)
                 {
                     row += 'B';
                 }
